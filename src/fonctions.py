feed267f6cbc260fb16a6ae73bfdd576c9227282
SuperSoja <vincent.amiot75@gmail.com>
2020-07-27 18:47:28 +0200

    dico

--- src/fonctions.py
+++ src/fonctions.py
@@ -6,6 +6,15 @@ Objectif : regrouper un ensemble de fonction permettant de hasher une chaine de 
 
 str = bytes("Hello", "utf-8")
 
+Alpha = {"a":0,"b":1,"c":2, "d":3,"e": 4, "f":5,"g" : 6,"h":7}
+def decalage(hash):
+    dict = {"a":1}
+    for i in hash:
+        pass
+    return dict
+
+str = "Hello"
+str = ' '.join(format(ord(x), 'b') for x in str).split(" ")
 def binary_to_hexa(bin):
     print(len(bin))
     for byte in bin:
