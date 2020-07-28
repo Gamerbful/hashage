0c541cf6b49fc2b1f8512c439f4ce2215e387324
Gamerbful <polyor56@gmail.com>
2020-07-28 11:56:12 +0200

    Bin to hexa and hexa to bin

--- src/fonctions.py
+++ src/fonctions.py
@@ -4,7 +4,6 @@ Devs: CAPELLE Bryan , AMIOT Vincent , DEHOUSSE Erwan
 Objectif : regrouper un ensemble de fonction permettant de hasher une chaine de caractere
 """
 
-str = bytes("Hello", "utf-8")
 
 Alpha = {"a":0,"b":1,"c":2, "d":3,"e": 4, "f":5,"g":6,"h":7,"i":8,"j":9,"k":10,"l":11,"m":12,"n":13,"o":14,"p":15,"q":16,"r":17,"s":18,"t":19,"u":20,"v":21,"w":22,"x":23,"y":24,"z":25,"A":26,"B":27,"C":28,"D":29,"E":30,"F":31,"G":32,"H":33,"I":34,"J":35,"K":36,"L":37,"M":38,"N":39,"O":40,"Q":41,"R":42,"S":43,"T":44,"U":45,"V":46,"W":47,"X":48,"Y":49,"Z":50,"0":51,"1":52,"2":53,"3":54,"4":55,"5":56,"6":57,"7":58,"8":59,"9":60," ":61}
 #Je n'ai pas ajouté les caractères spéciaux
@@ -39,21 +38,60 @@ des lettres)
 
 L'algorithme de Ceasar est dispo en python sur internet.
 """
-str = "Hello"
-str = ' '.join(format(ord(x), 'b') for x in str).split(" ")
+stri = "Janvier"
+stri = ' '.join(format(ord(x), 'b') for x in stri).split(" ")
+print(stri)
 def binary_to_hexa(bin):
+    hexa_list = []
+    hexa_carac = [ "A", "B", "C", "D", "E", "F"]
+    somme = 0
+    def inner_parse(hexa,somme):
+        two_power = [8,4,2,1]
+        for i in range(0,4):
+            if hexa[i] == "1":
+                somme += two_power[i]
+        if somme > 9:
+            somme -= 10
+            return hexa_carac[somme]
+        else:
+            return somme
     for byte in bin:
         hexa = "0"+byte
-        for i in range(0,4):
-            hexa[i]
-        print(hexa[4:8])
+        somme = 0
+        first = inner_parse(hexa[0:4],somme)
+        somme = 0
+        second = inner_parse(hexa[4:8],somme)
+        hexa_list.append(str(first)+str(second))
+    return hexa_list
 
-<<<<<<< HEAD
+def hexa_to_binary(hexa):
+    hexa_carac = { "A":"1010", "B":"1011", "C":"1100", "D":"1101", "E":"1110", "F":"1111"}
+    bin = []
+    for elt in hexa:
+        renvoi = ""
+        for j in range (0,2):
+            try:
+                number = int(elt[j])
+                add = "{0:b}".format(number)
+                longueur = len(add)
+                while longueur!=4:
+                    add = "0"+add
+                    longueur +=1
+                renvoi = renvoi + add
+            except:
+                renvoi = renvoi+hexa_carac[elt[j]]
+
+        bin.append(renvoi)
+    return bin
+
+
+
 def jules_cesar() :
     pass
 
-binary_to_hexa(str)
-=======
+print(binary_to_hexa(stri))
+print(hexa_to_binary(binary_to_hexa(stri)))
+
 def jules_cesar(chaine, decalage) :
     stri="" #initialisation de la chaine de caractere
     for i in range(0,len(chaine)):
@@ -62,4 +100,3 @@ def jules_cesar(chaine, decalage) :
         key_list = [k for (k, val) in Alpha.items() if val == lettre]
         stri += key_list[0]  # on recupere la liste (qui contient un seul element ) et on l'ajoute
     return stri
->>>>>>> 2e04fb346c4d6a98cdea5f676e2771808ff4918f
